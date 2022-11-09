07d3ec140021baae94b87c436cd27ca0b3ea012d
Nahlenko Oleksandr <noapioss@gmail.com>
2022-11-09 17:38:02 +0200

    delete photo command

--- Domain/Commands/DeletePhotoCommand.cs
+++ Domain/Commands/DeletePhotoCommand.cs
@@ -16,13 +16,12 @@ namespace Domain.Commands;
 public class DeletePhotoCommand : IRequest<DeletePhotoCommandResult>
 {
     public int DogId { get; init; }
-    public Stream PhotoStream { get; init; }
-    public string RootPath { get; init; }
+    public string PhotoPath { get; init; }
 }
 
 public class DeletePhotoCommandResult
 {
-    public bool PhotoIsAdded { get; init; }
+    public bool PhotoIsDeleted { get; init; }
 }
 
 internal class DeletePhotoCommandHandler : IRequestHandler<DeletePhotoCommand, DeletePhotoCommandResult>
@@ -35,55 +34,19 @@ internal class DeletePhotoCommandHandler : IRequestHandler<DeletePhotoCommand, D
     }
     public async Task<DeletePhotoCommandResult> Handle(DeletePhotoCommand request, CancellationToken cancellationToken = default)
     {
-        if (!await _dbContext.Images.AnyAsync(i => i.DogId == request.DogId, cancellationToken))
-        {
-            using (var fileStream = new FileStream($"{request.RootPath}\\images\\{request.DogId}\\1.jpg", FileMode.Create))
-            {
-                request.PhotoStream.Seek(0, SeekOrigin.Begin);
-                request.PhotoStream.CopyTo(fileStream);
-            }
-
-            var firstImage = new Image
-            {
-                DogId = request.DogId,
-                PhotoPath = $"/images/{request.DogId}/1.jpg"
-            };
-
-            await _dbContext.AddAsync(firstImage, cancellationToken);
-            await _dbContext.SaveChangesAsync(cancellationToken);
-
-            return new DeletePhotoCommandResult
-            {
-                PhotoIsAdded = true
-            };
-        }
-
-        var lastPhotoIndex = (await _dbContext.Images
-            .Where(i => i.DogId == request.DogId)
-            .Select(i => i.PhotoPath)
-            .MaxAsync(cancellationToken))
-            .Split('/').Last()
-            .Split('.').First();
-        
-        using (var fileStream = new FileStream($"{request.RootPath}\\images\\{request.DogId}\\{Int64.Parse(lastPhotoIndex)+1}.jpg", FileMode.Create))
-        {
-            request.PhotoStream.Seek(0, SeekOrigin.Begin);
-            request.PhotoStream.CopyTo(fileStream);
-        }
-
-
-        var image = new Image
+        var photo = new Image
         {
             DogId = request.DogId,
-            PhotoPath = $"/images/{request.DogId}/{Int64.Parse(lastPhotoIndex)+1}.jpg"
+            PhotoPath = request.PhotoPath
         };
 
-        await _dbContext.AddAsync(image, cancellationToken);
+        _dbContext.Attach(photo);
+        _dbContext.Remove(photo);
         await _dbContext.SaveChangesAsync(cancellationToken);
 
         return new DeletePhotoCommandResult
         {
-            PhotoIsAdded = true
+            PhotoIsDeleted = true
         };
     }
 }
