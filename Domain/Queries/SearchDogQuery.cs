462b74211dd93755119cd852eb9bd79ac4a86204
Nahlenko Oleksandr <noapioss@gmail.com>
2022-11-25 15:38:48 +0200

    search query update

--- Domain/Queries/SearchDogQuery.cs
+++ Domain/Queries/SearchDogQuery.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 
 using Contracts.Database;
 using Domain.Database;
+using System;
 
 namespace Domain.Queries;
 
@@ -37,15 +38,21 @@ internal class SearchGodQueryHandler : IRequestHandler<SearchGodQuery, SearchGod
     public async Task<SearchGodQueryResult> Handle(SearchGodQuery request, CancellationToken cancellationToken)
     {
         var searchRequest = string.IsNullOrEmpty(request.SearchRequest) ? "" : request.SearchRequest.ToLower();
+        var minBirthDate = DateTime.UtcNow.AddMonths(-request.MaxAge);
+        minBirthDate = minBirthDate.AddDays(-minBirthDate.Day);
+
         var dogs = await _dbContext.Doges
-            .Where(d => EF.Functions.Like(d.Name.ToLower(), $"%{searchRequest}%") ||
+            .Where(d => d.BirthDate.Date >= minBirthDate.Date &&
+                (request.Row == 0 || d.Row == request.Row) &&
+                (request.Enclosure == 0 || d.Enclosure == request.Enclosure) &&                    
+                (EF.Functions.Like(d.Name.ToLower(), $"%{searchRequest}%") ||
                 EF.Functions.Like(d.Breed.ToLower(), $"%{searchRequest}%") ||
                 EF.Functions.Like(d.Size.ToLower(), $"%{searchRequest}%") || 
-                EF.Functions.Like(d.About.ToLower(), $"%{searchRequest}%"))
+                EF.Functions.Like(d.About.ToLower(), $"%{searchRequest}%")))
             .Include(d => d.Photos)
             .OrderByDescending(d => d.Id)
             .ToListAsync(cancellationToken);
-
+        
         return new SearchGodQueryResult
         {
             Dogs = dogs
